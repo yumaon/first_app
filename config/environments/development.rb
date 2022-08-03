03a0e4bd4666e626a868bbaf304dedbf5eaf9dbd
yumaon <110388364+yumaon@users.noreply.github.com>
2022-08-03 15:09:51 +0000

    [Update]複数のホストからアクセスできるよう記述を変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,5 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.hosts << "16a7ef00d9af4c5fa7b4bf30f0e7e82b.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  config.hosts.clear
 end
